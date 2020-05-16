9b2992dfaa15b1737b9c07de263d3f4892f429ca
jared Higgins <hjared10@yahoo.com>
2020-05-15 21:57:25 -0600

    attempted to fix heroku errors again

--- routes/api/book.js
+++ routes/api/book.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const bookController = require("../../controllers/bookController");
 
-router.route("/").post(booksController.saveBooks);
+router.route("/").post(bookController.saveBooks);
 
 module.exports = router;
